282198394bfad120eccbfac8588aa58de13aa200
Dmitry Neverov <dmitry.neverov@jetbrains.com>
2022-01-23 09:14:17 +0100

    v1

--- src/main/java/sputnik/Sputnik.java
+++ src/main/java/sputnik/Sputnik.java
@@ -4,17 +4,13 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.Service;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.*;
 
 @Service
 public final class Sputnik implements Disposable {
@@ -22,12 +18,15 @@ public final class Sputnik implements Disposable {
   private final AtomicBoolean myStarted = new AtomicBoolean();
   private final AtomicBoolean myStop = new AtomicBoolean();
   private final ArrayBlockingQueue<Cmd> myCmds = new ArrayBlockingQueue<>(16 * 1024);
-  private final Hist myHist = new Hist();
-  private final AtomicReference<HistUi> myHistUi = new AtomicReference<>();
+
+  // hist name -> (bucket name -> hit count)
+  private final Map<String, Map<String, Integer>> myHists = new HashMap<>();
 
   private final Lock myUpdatedLock = new ReentrantLock();
-  private final Condition myUpdated  = myUpdatedLock.newCondition();
+  private final Condition myUpdated = myUpdatedLock.newCondition();
   private final AtomicInteger myUpdateCounter = new AtomicInteger();
+
+  private final ReadWriteLock myLock = new ReentrantReadWriteLock();
 
   void start() {
     if (myStarted.compareAndSet(false, true)) {
@@ -64,13 +63,56 @@ public final class Sputnik implements Disposable {
     myCmds.offer(new ClearCmd(histName));
   }
 
+  void delete(@NotNull String histName) {
+    myCmds.add(new DeleteCmd(histName)); // not offer, because we don't want to miss click on close in UI
+  }
+
   @Override
   public void dispose() {
     myStop.set(true);
   }
 
-  @Nullable HistUi getHist() {
-    return myHistUi.get();
+  @NotNull List<HistUi> getHist() {
+    Map<String, Map<String, Integer>> hists = new HashMap<>();
+    myLock.readLock().lock();
+    try {
+      for (String histName : myHists.keySet()) {
+        Map<String, Integer> buckets = myHists.get(histName);
+        hists.put(histName, new HashMap<>(buckets));
+      }
+    } finally {
+      myLock.readLock().unlock();
+    }
+
+    List<HistUi> histUis = new ArrayList<>();
+    for (Map.Entry<String, Map<String, Integer>> kv : hists.entrySet()) {
+      String histName = kv.getKey();
+      Map<String, Integer> histData = kv.getValue();
+
+      Hist hist = new Hist();
+      if (histData != null) {
+        hist.myMap.putAll(histData);
+      }
+
+      List<String> vals = new ArrayList<>(hist.myMap.keySet());
+      vals.sort((o1, o2) -> {
+        int i1 = hist.myMap.get(o1);
+        int i2 = hist.myMap.get(o2);
+        return -Integer.compare(i1, i2);
+      });
+      int[] counts = new int[vals.size()];
+      int total = 0;
+      for (int i = 0; i < counts.length; i++) {
+        int itemCount = hist.myMap.get(vals.get(i));
+        counts[i] = itemCount;
+        total += itemCount;
+      }
+
+      histUis.add(new HistUi(histName, vals, counts, total));
+    }
+
+    histUis.sort(Comparator.comparing(h -> h.myHistName));
+    return histUis;
   }
 
   private void processQueue() {
@@ -95,31 +137,27 @@ public final class Sputnik implements Disposable {
   }
 
   private void processCmds(@NotNull List<Cmd> cmds) {
-    for (Cmd cmd : cmds) {
-      if (cmd instanceof ClearCmd) {
-        myHist.myMap.clear();
+    myLock.writeLock().lock();
+    try {
+      for (Cmd cmd : cmds) {
+        if (cmd instanceof ClearCmd) {
+          Map<String, Integer> hist = myHists.get(((ClearCmd) cmd).myHistName);
+          if (hist != null) {
+            hist.clear();
+          }
+        }
+        else if (cmd instanceof HistCmd) {
+          HistCmd histCmd = (HistCmd) cmd;
+          Map<String, Integer> hist = myHists.computeIfAbsent(histCmd.myHistName, k -> new HashMap<>());
+          hist.merge(histCmd.myBucketName, 1, Integer::sum);
+        }
+        else if (cmd instanceof DeleteCmd) {
+          myHists.remove(((DeleteCmd) cmd).myHistName);
+        }
       }
-      if (cmd instanceof HistCmd) {
-        HistCmd histCmd = (HistCmd) cmd;
-        myHist.myMap.merge(histCmd.myBucketName, 1, Integer::sum);
-      }
+    } finally {
+      myLock.writeLock().unlock();
     }
-
-    List<String> vals = new ArrayList<>(myHist.myMap.keySet());
-    Collections.sort(vals, (o1, o2) -> {
-      int i1 = myHist.myMap.get(o1);
-      int i2 = myHist.myMap.get(o2);
-      return -Integer.compare(i1, i2);
-    });
-    int[] counts = new int[vals.size()];
-    int total = 0;
-    for (int i = 0; i < counts.length; i++) {
-      int itemCount = myHist.myMap.get(vals.get(i));
-      counts[i] = itemCount;
-      total += itemCount;
-    }
-
-    myHistUi.set(new HistUi(vals, counts, total));
 
     myUpdatedLock.lock();
     try {
@@ -135,21 +173,28 @@ public final class Sputnik implements Disposable {
   }
 
   static class HistUi {
+    private final String myHistName;
     private final List<String> myVals;
     private final int[] myCounts;
     private final int myTotal;
 
-    public HistUi(List<String> vals, int[] counts, int total) {
+    public HistUi(String histName, List<String> vals, int[] counts, int total) {
+      myHistName = histName;
       myVals = vals;
       myCounts = counts;
       myTotal = total;
+    }
+
+    @NotNull
+    String getHistName() {
+      return myHistName;
     }
 
     int getSize() {
       return myVals.size();
     }
 
-    String getVal(int i) {
+    String getBucketName(int i) {
       return myVals.get(i);
     }
 
@@ -167,10 +212,20 @@ public final class Sputnik implements Disposable {
 
   private static class ClearCmd implements Cmd {
     private final String myHistName;
+
     public ClearCmd(@NotNull String histName) {
       myHistName = histName;
     }
   }
+
+  private static class DeleteCmd implements Cmd {
+    private final String myHistName;
+
+    public DeleteCmd(@NotNull String histName) {
+      myHistName = histName;
+    }
+  }
+
   private static class HistCmd implements Cmd {
     private final String myHistName;
     private final String myBucketName;
--- src/main/java/sputnik/SputnikTW.java
+++ src/main/java/sputnik/SputnikTW.java
@@ -1,31 +1,40 @@
 package sputnik;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.ui.UISettings;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.JBColor;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import com.intellij.util.ui.JBFont;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SputnikTW implements ToolWindowFactory, DumbAware {
   @Override
   public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-    // 'clear histogram' button in ui
     Sputnik s = project.getService(Sputnik.class);
     SputnikPanel panel = new SputnikPanel(toolWindow, s);
-    ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-    Content content = contentFactory.createContent(panel, "", false);
-    content.setPreferredFocusedComponent(() -> panel);
+    JBScrollPane scrollPane = new JBScrollPane(panel,
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    Content content = ContentFactory.SERVICE.getInstance().createContent(scrollPane, "", false);
+    content.setPreferredFocusedComponent(() -> scrollPane);
     toolWindow.getContentManager().addContent(content);
     ApplicationManager.getApplication().executeOnPooledThread(() -> {
       int lastShown = 0;
@@ -40,18 +49,36 @@ public class SputnikTW implements ToolWindowFactory, DumbAware {
     private final AtomicBoolean myScheduled = new AtomicBoolean(false);
     private final ToolWindow myTw;
     private final Sputnik mySputnik;
-    private final JBFont myFont;
-    volatile Sputnik.HistUi myHist;
+    private final Font myFont;
+    private final Font myBoldFont;
+    private final List<Pair<Rectangle2D, String>> myCloseBounds = new ArrayList<>();
 
     public SputnikPanel(@NotNull ToolWindow tw, @NotNull Sputnik sputnik) {
       myTw = tw;
       mySputnik = sputnik;
       myFont = JBUI.Fonts.create(Font.MONOSPACED, 11);
+      myBoldFont = myFont.deriveFont(Font.BOLD);
+
+      addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+          super.mouseClicked(e);
+          Point point = e.getPoint();
+          for (Pair<Rectangle2D, String> b : myCloseBounds) {
+            if (b.first.contains(point)) {
+              ApplicationManager.getApplication().executeOnPooledThread(() -> {
+                mySputnik.delete(b.second);
+                scheduleRepaint();
+              });
+              break;
+            }
+          }
+        }
+      });
     }
 
     void scheduleRepaint() {
       if (myScheduled.compareAndSet(false, true)) {
-        myHist = mySputnik.getHist();
         repaint();
       }
     }
@@ -59,35 +86,85 @@ public class SputnikTW implements ToolWindowFactory, DumbAware {
     protected void paintComponent(Graphics g) {
       super.paintComponent(g);
       myScheduled.set(false);
+      List<Sputnik.HistUi> myHists = mySputnik.getHist();
 
       UISettings.setupAntialiasing(g);
       g.setFont(myFont);
 
-      int ystart = 30;
+      int y = 30;
+      for (Sputnik.HistUi hist : myHists) {
+        y = drawHist(g, y, hist, true);
+        y += 20;
+      }
 
-      Sputnik.HistUi hist = this.myHist;
-      if (hist != null) {
-        int size = hist.getSize();
-        int total = hist.getTotal();
-        int totalWidth = 100;
-        for (int i = 0; i < size; i++) {
-          String val = hist.getVal(i);
-          int count = hist.getCount(i);
-          int width = (int) (totalWidth * (count * 1.0 / total));
-          int y = ystart + 20 * i;
-          if (width == 0) {
-            g.drawString(i > 0 ? "rest is smaller than 1%" : "all is smaller than 1%, total: " + total,
-                    10, y + 10);
-            break;
+      Dimension size = getSize();
+      if (y != size.height) {
+        setPreferredSize(new Dimension(size.width, y));
+        revalidate();
+      }
+
+      myCloseBounds.clear();
+
+      y = 30;
+      for (Sputnik.HistUi hist : myHists) {
+        y = drawHist(g, y, hist, false);
+        y += 20;
+      }
+    }
+
+    private int drawHist(Graphics g, int y, Sputnik.HistUi hist, boolean dryRun) {
+      int size = hist.getSize();
+      int total = hist.getTotal();
+      float k = 1.0f / total;
+
+      int totalWidth = 100;
+      int rowHeight = 10;
+
+      if (!dryRun) {
+        String namePrefix = hist.getHistName();
+        if (!namePrefix.isEmpty()) {
+          namePrefix += ", ";
+        }
+        String title = namePrefix + "total: " + total;
+        TextLayout tl = new TextLayout(title, myBoldFont, ((Graphics2D)g).getFontRenderContext());
+        tl.draw((Graphics2D)g, 10, y);
+        Rectangle2D bounds = tl.getBounds();
+        bounds.setRect(10 + (int) bounds.getMaxX() + 5,
+                bounds.getY() + y - 1 - ((AllIcons.Actions.Close.getIconHeight() - rowHeight)/2.0),
+                AllIcons.Actions.Close.getIconWidth(),
+                AllIcons.Actions.Close.getIconHeight());
+
+        myCloseBounds.add(Pair.create(bounds, hist.getHistName()));
+        AllIcons.Actions.Close.paintIcon(this, g, (int)bounds.getX(), (int)bounds.getY());
+      }
+
+      y += rowHeight;
+
+      for (int i = 0; i < size; i++) {
+        int count = hist.getCount(i);
+        float bucketContrib = count * k;
+        if (bucketContrib < 0.01) {
+          if (!dryRun) {
+            g.drawString((i > 0 ? "rest" : "all") + " are smaller than 1%", 10, y + rowHeight);
           }
+          y += 2 * rowHeight;
+          break;
+        }
+
+        if (!dryRun) {
+          int width = (int) (totalWidth * bucketContrib);
           Color color = g.getColor();
           g.setColor(JBColor.RED);
-          g.fillRect(10, y, width, 10);
+          g.fillRect(10, y, width, rowHeight);
           g.setColor(color);
-          val = val + " " + count + "/" + total + " (" + (int)(count * 100.0 / total) + "%)";
-          g.drawString(val, width + 20, y + 10);
+
+          String bucketText = hist.getBucketName(i) + " " + count + " (" + (int) (100 * bucketContrib) + "%)";
+          g.drawString(bucketText, width + 20, y + rowHeight);
         }
+        y += 2 * rowHeight;
       }
+
+      return y;
     }
   }
 }
